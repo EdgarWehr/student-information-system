98336cb6d07ac3268d9cb50a0550e3e630ac4fb7
Edgar Alberto Flores Wehr <wehredgar@gmail.com>
2019-08-29 10:49:37 -0500

    Test create for Student Class

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student
+{
+    private String myName;
+    
+    public Student(String name)
+    {
+        myName = name;
+    }
+    
+    public String getName()
+    {
+        return myName;
+    }
+}
---
+++ StudentTest.java
@@ -0,0 +1,54 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class StudentTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class StudentTest
+{
+    /**
+     * Default constructor for test class StudentTest
+     */
+    public StudentTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testCreate()
+    {
+        Student student1 = new Student("Jane Doe");
+        String studentName = student1.getName();
+        assertEquals("Jane Doe",studentName);
+        
+        Student student2 = new Student("Joe Black");
+        studentName = student2.getName();
+        assertEquals("Joe Black", studentName);
+    }
+}
