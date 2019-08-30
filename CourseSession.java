b14e3baa177f279bca200d24a10411c293ba2a90
Edgar Alberto Flores Wehr <wehredgar@gmail.com>
2019-08-30 11:01:20 -0500

    hicimos el metodo de enrolll que mete un estudiante en un curso

---
+++ CourseSession.java
@@ -0,0 +1,41 @@
+public class CourseSession
+{
+    private String department;
+    private String number;
+    private int numberStudents;
+    private Student[] allStudents;
+    
+    public CourseSession(String department, String number)
+    {
+        this.department = department;
+        this.number = number;
+        this.numberStudents = 0;
+        this.allStudents = new Student[20];
+    }
+    
+    public String getDepartment()
+    {
+        return this.department;
+    }
+    
+    public String getNumber()
+    {
+        return this.number;
+    }
+    
+    public int getNumberOfStudents()
+    {
+        return numberStudents;
+    }
+    
+    public void enroll(Student studentN)
+    {
+        allStudents[numberStudents] = studentN;
+        numberStudents += 1;
+    }
+    
+    public Student[] getAllStudents()
+    {
+        return allStudents;
+    }
+}
---
+++ CourseSessionTest.java
@@ -0,0 +1,70 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class CourseSessionTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CourseSessionTest
+{
+    /**
+     * Default constructor for test class CourseSessionTest
+     */
+    public CourseSessionTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testCreate()
+    {
+        CourseSession session1 = new CourseSession("ENGL","101");
+        assertEquals("ENGL", session1.getDepartment());
+        assertEquals("101", session1.getNumber());
+        assertEquals(0,session1.getNumberOfStudents());
+    }
+    
+    @Test
+    public void testRollStudent()
+    {
+        CourseSession session1 = new CourseSession("ENGL","101");
+        
+        Student student1 = new Student("Cain DiVoe");
+        
+        session1.enroll( student1 );
+        assertEquals(1, session1.getNumberOfStudents());
+        
+        Student student2 = new Student("Carol DeVaughne");
+        
+        session1.enroll( student2 );
+        assertEquals(2, session1.getNumberOfStudents());
+        
+        assertEquals(student1, session1.getAllStudents()[0]);
+        assertEquals(student2, session1.getAllStudents()[1]);
+    }
+}
